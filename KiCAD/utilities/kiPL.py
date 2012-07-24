6b129261ac2149f7ef4d5259cf1433da3c5380f1
doug@douglasgilliland.com <doug@douglasgilliland.com>
2012-07-24 13:39:51 -0400

    updating the DS18S20 design

--- KiCAD/utilities/kiPL.py
+++ KiCAD/utilities/kiPL.py
@@ -1,7 +1,6 @@
-# kiPL.py
+# DupsCheck.py
 """
-KiCad BOM converter - Read in a Parts List created by KiCad and convert it to a format
-that can more easily be used by BOM processors, such as Mouser's input format.
+Duplicates checker.
 """
 
 import csv
@@ -11,11 +10,11 @@ import os
 
 from sys import argv
 
-print 'kiPL.py'
+print 'DupsCheck.py'
 
 if (len(sys.argv) != 2):
   print len(sys.argv)
-  print 'kiPL.py - Usage kiPL.py filename.csv'
+  print 'DupsCheck.py - Usage DupsCheck.py filename.csv'
   print 'Exiting...'
   exit()
 
@@ -29,7 +28,7 @@ if fileToRead.upper()[-3:] != 'CSV':
 else:
 	print 'Input file is a CSV file as expected'
 
-fileToWrite = fileToRead[:-4] + "_mouser.csv"
+fileToWrite = fileToRead[:-4] + "_duplicates.csv"
 
 try:
 	inCSVFile = csv.reader(open(fileToRead, 'rb'), delimiter='\t')
@@ -43,55 +42,32 @@ except IOError:
 	print 'ERROR - Cannot open the output file', fileToRead,
 	print ' - Is the file already open in EXCEL?'
 	exit()
-	
-firstCSVRow = inCSVFile.next()
-#print 'first row is', firstCSVRow
-if firstCSVRow[0] != 'ref':
-	print 'ERROR - Expected a ref as the first element'
-	exit()
-outCSVRow = []
-outCSVRow.append("Mfg Part Number")
-outCSVRow.append("Quantity 1")
-outCSVFile.writerow(outCSVRow)	# write out the header
 
-partsList = []
+readCSVRow = inCSVFile.next()
+print 'first row is', readCSVRow
+readString = readCSVRow[0]
+if readString[0] != 'V':
+	print 'ERROR - Expected Volume as the first line'
+	exit()
+
+inCSVFile.next()  # blank line next
+
+readCSVRow = inCSVFile.next()
+if readCSVRow[0] != 'Date':
+	print 'ERROR - Expected Date as the third line'
+	exit()
+
+filesList = []
 
 # ['ref', 'value', 'footprint', 'Field1', 'Field2', 'Field3', 'Field4']
 
 for row in inCSVFile:
-  outCSVRow = row
-  partsList.append(row)
+  filesList.append(row)
 
-#print partsList
+print filesList
 
-print 'Parts List is', len(partsList),
-print 'parts long'
-
-parts = []
-for row in partsList:
-	if row[4] not in parts:
-		parts.append(row[4])
-
-print 'There are ', len(parts),
-
-print 'unique parts'
-# print 'Unique parts are', parts
-
-qtys = []
-partNum = 0
-for part in parts:
-	qtys.append(0)
-	for inPart in partsList:
-		if part == inPart[4]:
-			qtys[partNum] += 1
-	partNum += 1
-
-numParts = len(parts)
-loopCount = 0
-while loopCount < numParts:
-	outCSVRow = []
-	outCSVRow.append(parts[loopCount])
-	outCSVRow.append(qtys[loopCount])
-	outCSVFile.writerow(outCSVRow)
-	loopCount += 1
+#parts = []
+#for row in filesList:
+#	if row[3] not in parts:
+#		parts.append(row[4])
 
