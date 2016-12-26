78dee0fde28cfdc79d7805a2fbe43b8c92150d83
Doug Gilliland <doug@douglasgilliland.com>
2016-12-25 20:31:40 -0500

    sd card

---
+++ KiCAD/utilities/padSolver/padSolver.py
@@ -0,0 +1,52 @@
+p6x=0.0
+p7x=p6x-2.42
+p8x=p6x-4.12
+p5x=p6x+2.5
+p4x=p5x+2.5
+p3x=p4x+2.5
+p2x=p3x+2.5
+p1x=p2x+2.5
+p9x=p1x+2.5
+p11x=p6x-6.66
+p10x=p6x-5.46
+p12x=p9x+2.07
+p13x=p12x-26.6+1.5
+parb1=p4x+.62
+p16x=parb1-11.5
+p17x=p16x+21
+p14x=parb1-13.65
+p15x=p14x+27.3
+p16y=0
+p17y=p16y
+p11y=p16y-2.05-.75
+p10y=p11y
+p9y=p10y
+p8y=p9y
+p7y=p8y
+p6y=p7y
+p5y=p6y
+p4y=p5y
+p3y=p4y
+p2y=p3y
+p1y=p2y
+p12y=p16y-2.4+1.4
+p13y=p12y
+p15y=p12y+14.5
+p14y=p15y
+print 'p1(x,y)',p1x,p1y
+print 'p2(x,y)',p2x,p2y
+print 'p3(x,y)',p3x,p3y
+print 'p4(x,y)',p4x,p4y
+print 'p5(x,y)',p5x,p5y
+print 'p6(x,y)',p6x,p6y
+print 'p7(x,y)',p7x,p7y
+print 'p8(x,y)',p8x,p8y
+print 'p9(x,y)',p9x,p9y
+print 'p10(x,y)',p10x,p10y
+print 'p11(x,y)',p11x,p11y
+print 'p12(x,y)',p12x,p12y
+print 'p13(x,y)',p13x,p13y
+print 'p14(x,y)',p14x,p14y
+print 'p15(x,y)',p15x,p15y
+print 'p16(x,y)',p16x,p16y
+print 'p17(x,y)',p17x,p17y
